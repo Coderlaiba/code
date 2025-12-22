b5ab5daf102b3bea8370b5c9bdd9b75ca17d297c
Coderlaiba <perweenlaiba910@gmail.com>
2025-12-22 17:14:25 +0530

    Time: 2 ms (82.56%) | Memory: 13.6 MB (34.36%) - LeetSync

---
+++ 1000-delete-columns-to-make-sorted-iii/delete-columns-to-make-sorted-iii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int m = strs.size();
+        int n = strs[0].size();
+
+        vector<int> dp(n, 1);
+        int longest = 1;
+
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < j; i++) {
+                bool valid = true;
+                for (int r = 0; r < m; r++) {
+                    if (strs[r][i] > strs[r][j]) {
+                        valid = false;
+                        break;
+                    }
+                }
+                if (valid) {
+                    dp[j] = max(dp[j], dp[i] + 1);
+                }
+            }
+            longest = max(longest, dp[j]);
+        }
+
+        return n - longest;
+    }
+};
