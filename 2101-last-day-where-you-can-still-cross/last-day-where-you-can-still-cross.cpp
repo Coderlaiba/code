d427812cb588b8f84bfce03be6f34a8c380033d5
Coderlaiba <perweenlaiba910@gmail.com>
2026-01-01 01:37:28 +0530

    Time: 264 ms (42.14%) | Memory: 194 MB (51.05%) - LeetSync

---
+++ 2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    int row, col;
+    vector<vector<int>> dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
+    bool canCross(int day, vector<vector<int>>& cells) {
+        vector<vector<int>> grid(row, vector<int>(col, 0));
+
+        // Flood cells up to 'day'
+        for (int i = 0; i < day; i++) {
+            grid[cells[i][0] - 1][cells[i][1] - 1] = 1;
+        }
+
+        queue<pair<int,int>> q;
+
+        // Start BFS from top row
+        for (int j = 0; j < col; j++) {
+            if (grid[0][j] == 0) {
+                q.push({0, j});
+                grid[0][j] = 1;
+            }
+        }
+
+        while (!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop();
+
+            if (x == row - 1) return true;
+
+            for (auto &d : dirs) {
+                int nx = x + d[0];
+                int ny = y + d[1];
+
+                if (nx >= 0 && ny >= 0 && nx < row && ny < col && grid[nx][ny] == 0) {
+                    grid[nx][ny] = 1;
+                    q.push({nx, ny});
+                }
+            }
+        }
+
+        return false;
+    }
+
+    int latestDayToCross(int r, int c, vector<vector<int>>& cells) {
+        row = r;
+        col = c;
+
+        int low = 1, high = r * c, ans = 0;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (canCross(mid, cells)) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
